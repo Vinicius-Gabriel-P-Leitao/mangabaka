1736923e20a893cd78da3a93f4871c46ca4b9855
Vinicius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2025-06-12 23:15:34 -0300

    [feat]: Pagina de notfound já feita e funcionando perfeitamente.

--- app/frontend/mangabaka/src/view/NotFound.vue
+++ app/frontend/mangabaka/src/view/NotFound.vue
@@ -14,11 +14,29 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="not-found text-center mt-16">
-    <img :src="frieren" alt="frieren sleppy 404 error" class="w-2xl" />
+  <main class="grid grid-flow-row grid-cols-1 gap-5 place-items-center">
+    <section class="not-found text-center sm:text-left grid gap-5 grid-rows-2 sm:grid-rows-1 sm:grid-cols-2">
+      <div class="grid grid-rows-1 grid-cols-3 gap-3">
+        <span class="content-center text-end text-7xl sm:text-9xl" aria-hidden="true">4</span>
+        <span class="place-self-center">
+          <img :src="frieren" alt="Frieren triste por erro 404"
+            class="w-24 sm:w-40 h-24 sm:h-40 object-cover rounded-full shadow-lg border-4 border-white" />
+        </span>
+        <span class="content-center text-start text-7xl sm:text-9xl" aria-hidden="true">4</span>
+      </div>
 
-    <h1>404 - Página não encontrada 😢</h1>
-    <p>Não foi posivel encontrar: <strong>{{ originalPath }}</strong></p>
-    <router-link to="/">Voltar pra Home</router-link>
-  </div>
+      <section class="place-self-center">
+        <p class="mb-2 sm:mb-6 text-2xl sm:text-5xl text-gray-600">Página não encontrada 😢</p>
+        <p class="text-xs sm:text-2xl">
+          Não foi possível encontrar: <strong>{{ originalPath }}</strong>
+        </p>
+      </section>
+    </section>
+
+    <nav>
+      <button class="btn-primary">
+        <router-link to="/">Voltar pra Home</router-link>
+      </button>
+    </nav>
+  </main>
 </template>
