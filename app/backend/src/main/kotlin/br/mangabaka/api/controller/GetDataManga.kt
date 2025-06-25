940cbe4e397b7bc591f265f930bf7f7a6d552194
Vinicius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2025-06-25 12:13:03 -0300

    [fixed]: Corrije no backend e root Copyright de {year} para o ano correto.

--- app/backend/src/main/kotlin/br/mangabaka/api/controller/GetDataManga.kt
+++ app/backend/src/main/kotlin/br/mangabaka/api/controller/GetDataManga.kt
@@ -1,11 +1,10 @@
 /*
  * SPDX-License-Identifier: BSD-3-Clause
  *
- * Copyright (c) {year} Vinícius Gabriel Pereira Leitão
+ * Copyright (c) 2025 Vinícius Gabriel Pereira Leitão
  * Licensed under the BSD 3-Clause License.
  * See LICENSE file in the project root for full license information.
  */
-
 package br.mangabaka.api.controller
 
 import br.mangabaka.api.dto.AssetType
