2693e35cc0b2ce700cd9817591cfc915613597f6
Vinicius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2025-06-27 18:25:17 -0300

    [feat]: Implementa tradução de handlers

--- app/frontend/mangabaka/src/application/error/handler/ApiErrorMessageHandler.ts
+++ app/frontend/mangabaka/src/application/error/handler/ApiErrorMessageHandler.ts
@@ -3,14 +3,17 @@
 // Copyright (c) 2025 Vinícius Gabriel Pereira Leitão
 // Licensed under the BSD 3-Clause License.
 // See LICENSE file in the project root for full license information.
-import type { ApiErrorCode } from "../code/base/ApiErrorCode";
-import type { ErrorCodeProvider } from "../code/ErrorCodeProvider";
 import i18n from "@/domain/config/I18n";
+import { ErrorCodes } from "@/export";
+import type { ErrorCodeProvider } from "../code/ErrorCodeProvider";
 
 const t = i18n.global.t;
 
 // prettier-ignore
-export const ApiErrorMessageHandler: Record<ApiErrorCode, ErrorCodeProvider> = {
+export const ApiErrorMessageHandler: Record<
+  ErrorCodes.ApiErrorCode,
+  ErrorCodeProvider
+> = {
   NOT_FOUND: {
     code: "NOT_FOUND",
     handle: (param: any): string => {
@@ -21,25 +24,28 @@ export const ApiErrorMessageHandler: Record<ApiErrorCode, ErrorCodeProvider> = {
   BAD_REQUEST: {
     code: "BAD_REQUEST",
     handle: (param: any): string => {
-      if (param?.field) return `Campo inválido: ${param.field}.`;
-      return "A requisição está mal formada. Verifique os dados enviados.";
+      if (param?.field)
+        return `${t("handler.bad_request.invalid_fiel")} ${param.field}.`;
+      return t("handler.bad_request.malformed_request");
     },
   },
   BAD_GATEWAY: {
     code: "BAD_GATEWAY",
     handle: (param: any): string => {
       if (param?.status === 502)
-        return "Gateway inválido: servidor intermediário falhou.";
+        return t("handler.bad_gateway.invalid_gateway");
 
-      if (param?.status === 504) return "Timeout do servidor de origem.";
+      if (param?.status === 504)
+        return t("handler.bad_gateway.gateway_timeout");
 
-      return `Erro ao acessar o servidor intermediário. Código: 
-      ${param?.status ?? "desconhecido"}`;
+      return `${t("handler.bad_gateway.intermediary_server")} ${
+        param?.status ?? t("handler.unknown.unknown")
+      }`;
     },
   },
   GATEWAY_TIMEOUT: {
     code: "GATEWAY_TIMEOUT",
     handle: (_param: any): string =>
-      "O servidor demorou demais para responder. Tente novamente mais tarde.",
+      t("handler.gateway_timeout.try_again_later"),
   },
 };
