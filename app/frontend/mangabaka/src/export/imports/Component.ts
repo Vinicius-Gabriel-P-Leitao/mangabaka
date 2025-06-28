702f8a825077a0d4ec22776e54c8b8fc65a2696b
Vinicius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2025-06-28 05:10:47 -0300

    [chore]: Update nas licenças do frontend.

--- app/frontend/mangabaka/src/export/imports/Component.ts
+++ app/frontend/mangabaka/src/export/imports/Component.ts
@@ -3,16 +3,10 @@
 // Copyright (c) 2025 Vinícius Gabriel Pereira Leitão
 // Licensed under the BSD 3-Clause License.
 // See LICENSE file in the project root for full license information.
-import DropdownTranslation from "@/view/component/Select.vue";
-import PluginTranslation from "@/view/component/Translation.vue";
-import BaseButton from "@/view/component/Button.vue";
+import Select from "@/view/component/Select.vue";
+import Translation from "@/view/component/Translation.vue";
+import Button from "@/view/component/Button.vue";
 import InfoView from "@/view/component/InfoView.vue";
-import ToastError from "@/view/component/Toast.vue";
+import Toast from "@/view/component/Toast.vue";
 
-export {
-  DropdownTranslation,
-  PluginTranslation,
-  BaseButton,
-  InfoView,
-  ToastError,
-};
+export { Select, Translation, Button, InfoView, Toast };
--- app/frontend/mangabaka/src/export/index.ts
+++ app/frontend/mangabaka/src/export/index.ts
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright (c) 2025 Vinícius Gabriel Pereira Leitão
+// Licensed under the BSD 3-Clause License.
+// See LICENSE file in the project root for full license information.
 import * as Components from "./imports/Component";
 import * as ErrorCodes from "./imports/ErrorCode";
 import * as Exceptions from "./imports/Exception";
